bc26a241d52626f339f778d886e7449262cdba5b
Amit Ojha <ojhaamit@Amits-Laptop.local>
2025-01-05 06:09:33 -0800

    Group Anagram

---
+++ GroupAnagrams.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+    
+    private static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+
+            map.get(sortedStr).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
