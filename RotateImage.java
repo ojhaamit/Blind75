b97fda44f39213b72d085e8aeed6c06d9fa5f262
Amit Ojha <ojhaamit@Amits-Laptop.local>
2025-01-04 20:08:43 -0800

    Rotate Matrix Changes

--- RotateImage.java
+++ RotateImage.java
@@ -13,11 +13,49 @@ public class RotateImage {
         int row = matrix.length;
         int col = matrix[0].length;
 
+        // Print the matrix
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
+        }
+
+        // Reverse the matrix vertically
+        reverse(matrix);
+
+        // Print the reversed matrix
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Print the matrix
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void reverse(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - 1 - i];
+            matrix[n - 1 - i] = temp;
         }
     }
 }
